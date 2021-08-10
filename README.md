1922f5888c8826df6bd2857821419d0b9619573e
joanSolCom <joan.soler.company@gmail.com>
2021-08-10 12:35:13 +0200

    Main commit IS2021

--- README.md
+++ README.md
@@ -1,1 +1,84 @@
 # ThemePro_2.0
+
+This code is part of a scientific publication presented in the Show&tell track of INTERSPEECH 2021. It is a second iteration of a previous demo published in LREC2020 that could not be presented due to the conference cancellation for the COVID-19 pandemic.
+
+Please, cite these papers if you use this code:
+
+Dominguez, M., Soler, J., & Wanner, L. (2021). ThemePro: 2.0: Showcasing the Role of Thematic Progression in Engaging Human-Computer Interaction. In Proceedings of INTERSPEECH, Brno, Czechia.
+
+Dominguez, M., Soler, J., & Wanner, L. (2020). ThemePro: A Toolkit for the Analysis of Thematic Progression. In Proceedings of The 12th Language Resources and Evaluation Conference (pp. 1000-1007).
+
+To run the demo, you will need a working Docker installation to run the TTS application (See https://www.docker.com/get-started).
+
+The website runs locally on your computer. Please, make sure you follow the installation instructions below. 
+
+WARNING: Spacy and neuralcoref versions may cause failure if they do not meet these requirements.
+
+## Installation instructions
+
+```
+apt-get update
+apt-get install build-essential python3 python3-dev python3-pip git -y
+apt-get upgrade -y
+pip3 install -U pip setuptools wheel
+pip3 install -U spacy==2.3.1
+pip3 install numpy scipy flask flask-jsonpify gensim
+python3 -m spacy download en_core_web_lg-2.3.1 --direct
+
+git clone https://github.com/huggingface/neuralcoref.git
+cd neuralcoref && pip3 install -r requirements.txt && pip3 install -e .
+
+git clone https://github.com/monikaUPF/ThemePro_2.0.git 
+cd ThemePro_2.0/backend/embeddings && wget https://s3.amazonaws.com/dl4j-distribution/GoogleNews-vectors-negative300.bin.gz
+mv GoogleNews-vectors-negative300.bin.gz google.bin.gz
+cd ThemePro_2.0/frontend/ && mkdir /var/www/html/themePro/ && cp -R * /var/www/html/themePro/
+pip3 install sklearn
+pip3 install flask_cors
+pip3 install python-Levenshtein
+```
+
+## Running the demo
+
+1) Navigate to the ThemePro_2.0/ folder in your computer, download the TTS repository and run the docker:
+
+```
+git clone https://github.com/synesthesiam/docker-mozillatts.git
+cd /[local_path_to_folder]/ThemePro_2.0/docker-mozillatts
+sudo docker run -it -p 5002:5002 synesthesiam/mozillatts:en
+```
+
+* Wait until you see this message at the bottom of terminal ouput:
+ > Generator Model: multiband_melgan_generator
+ * Serving Flask app "mozillatts" (lazy loading)
+ * Environment: production
+   WARNING: This is a development server. Do not use it in a production deployment.
+   Use a production WSGI server instead.
+ * Debug mode: off
+[INFO]  * Running on http://0.0.0.0:5002/ (Press CTRL+C to quit)
+
+
+2) Open a second terminal and change directory to the backend folder inside ThemePro_2.0. Then, run the main script called "themazo.py":
+
+```
+cd /[local_path_to_folder]/ThemePro_2.0/backend
+python3 themazo.py
+```
+* Embeddings take a short while to load. Please, be patient and wait until you see these lines at the bottom of the terminal output:
+loading spacy
+loaded
+loading embeddings
+loaded
+ * Serving Flask app "themazo" (lazy loading)
+ * Environment: production
+   WARNING: This is a development server. Do not use it in a production deployment.
+   Use a production WSGI server instead.
+ * Debug mode: on
+ * Running on http://0.0.0.0:5000/ (Press CTRL+C to quit)
+
+
+3) Open a tab in your browser and go to http://localhost/ThemePro
+
+4) Copy-paste or write a text in the main box and click on run. Then, navigate to the desired tag to see the output.
+
+* Remember you must click on the Synth button to call the TTS and thus generate the speech output.
+
---
+++ backend/themazo.py
@@ -0,0 +1,144 @@
+from flask_jsonpify import jsonify
+import json
+from flask import Flask, app, request, url_for, Response
+from logging.handlers import RotatingFileHandler
+from logging import Formatter, INFO
+import spacy
+from themaParse import ThemParser
+from themaProg import ThematicProgression
+import neuralcoref
+import gensim
+from flask import g
+from flask_cors import CORS
+from blockTTS import BlockTTS
+
+app = Flask(__name__)
+CORS(app)
+
+cache = {}
+
+def load_spacy():
+	print("loading spacy")
+	en_nlp = spacy.load('en_core_web_lg')
+	neuralcoref.add_to_pipe(en_nlp)
+
+	print("loaded")
+	return en_nlp
+
+def load_embeddings():
+    print("loading embeddings")
+    model = gensim.models.KeyedVectors.load_word2vec_format('./embeddings/google.bin.gz', binary=True)
+    print("loaded")
+    return model
+
+en_nlp = load_spacy()
+model = load_embeddings()
+
+def process(text):
+    text = text.replace("  "," ")
+    doc = en_nlp(text)
+
+    textConll = ""
+    sents = []
+    for sent in doc.sents:
+        print(sent)
+        s = []
+        sentConll = ""
+        for token in sent:
+            if token.text.strip():
+                s.append(token.text)
+                lineConll = str(token.i - sent.start + 1)+"\t"+token.text+"\t"+token.lemma_+"\t"+token.tag_+"\t"+token.dep_+"\t"+str(token.head.i - sent.start + 1)+"\n"
+                sentConll+=lineConll
+
+        sents.append(s)
+        textConll+=sentConll+"\n"
+
+    cache["sentences"] = sents
+
+    sentences = textConll.strip().split("\n\n")
+    dictJson = {}
+    dictJson["raw"] = textConll
+    dictJson["sentences"] = []
+    for sentence in sentences:
+        dictS = {}
+        dictS["sentence"] = sentence
+        dictS["tokens"] = []
+        for token in sentence.split("\n"):
+            dictS["tokens"].append(token)
+
+        dictJson["sentences"].append(dictS)
+
+    dictJson["corefs"] = []
+
+    for coref in doc._.coref_clusters:
+        strCor = str(coref).lower()
+        root = strCor.split(":")[0]
+        chain = strCor.split(":")[1].replace("[","").replace("]","").strip().split(", ")[1:]
+        chain.insert(0,root)
+
+        dictJson["corefs"].append(chain)
+
+    cache["corefs"] = dictJson["corefs"]
+
+    jsonStr = json.dumps(dictJson)
+    return jsonify(jsonStr), textConll
+
+
+@app.route('/getConll', methods=['POST'])
+def getConll():
+    arguments = request.form
+    text = arguments["text"].strip().replace('"',"").replace("'","")
+    jsonStr, textConll = process(text)
+    cache["conll"] = textConll
+    return jsonStr
+
+@app.route("/getThematicity" , methods=['POST'])
+def getThematicity():
+    iT = ThemParser(raw_conll=cache["conll"])
+    cache["iT"] = iT
+
+    levels = iT.levels
+    sentences = cache["conll"].strip().split("\n\n")
+    dictJson = {}
+    dictJson["sentences"] = []
+
+    for idS, sentence in enumerate(sentences):
+        dictS = {}
+        dictS["text"] = ""
+        dictS["tokens"] = []
+        dictS["pos"] = []
+        dictS["components"] = []
+        for token in sentence.split("\n"):
+            tokenComponents = token.split("\t")
+            if len(tokenComponents) > 1:
+                dictS["text"] += tokenComponents[1] + " "
+                dictS["tokens"].append(tokenComponents[1])
+                dictS["pos"].append(tokenComponents[3])
+
+        dictS["text"] = dictS["text"].strip()
+        if levels:
+            dictS["components"] = levels[idS]
+
+        dictJson["sentences"].append(dictS)
+        
+    jsonStr = json.dumps(dictJson)
+    return jsonify(jsonStr)
+
+@app.route('/getThematicProgression', methods=["POST"])
+def getThematicProgression():
+	return jsonify("")
+
+@app.route('/getBlocks', methods=['POST'])
+def getBlocks():
+    iB = BlockTTS(cache["corefs"], cache["iT"], model, cache["sentences"])
+    dictJSON = {}
+    dictJSON["blocks"] = iB.blocks
+    jsonStr = json.dumps(dictJSON)
+    return jsonify(jsonStr)
+
+
+if __name__ == '__main__':
+
+    app.config['PROPAGATE_EXCEPTIONS'] = True
+    app.config['CORS_HEADERS'] = 'Content-Type'
+    app.run(host= '0.0.0.0', port=5000, debug=True, use_reloader=False)
